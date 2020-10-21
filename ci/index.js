5c5d9338e39f6b406405d7c88dfb1d95f835a877
woaidouya123 <cfeigood@qq.com>
2020-10-21 14:20:41 +0800

    添加自动化构建

---
+++ ci/index.js
@@ -0,0 +1,28 @@
+const ci = require('miniprogram-ci')
+;(async () => {
+  const project = new ci.Project({
+    appid: 'wxac429285b3099f2e',
+    type: 'miniProgram',
+    projectPath: 'dist/dev/mp-weixin',
+    privateKeyPath: '/root/wechat_key/private.wxac429285b3099f2e.key',
+    ignores: ['node_modules/**/*'],
+  })
+  const uploadResult = await ci.upload({
+    project,
+    version: `1.0.${BUILD_NUMBER}`,
+    desc: `${CHANGE_TITLE}`,
+    onProgressUpdate: console.log,
+  })
+  console.log(uploadResult)
+  const previewResult = await ci.preview({
+    project,
+    version: `1.0.${BUILD_NUMBER}`,
+    desc: `${CHANGE_TITLE}`,
+    qrcodeFormat: 'image',
+    qrcodeOutputDest: 'ci/destination.jpg',
+    onProgressUpdate: console.log,
+    // pagePath: 'pages/index/index', // 预览页面
+    // searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
+  })
+  console.log(previewResult)
+})()
